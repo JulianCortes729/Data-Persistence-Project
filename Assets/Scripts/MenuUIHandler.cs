5f6b152f86fa4519da0fc69531d47c5ef7333e2f
Julian Cortes <juliancortes729@gmail.com>
2024-02-16 20:47:54 -0300

    third commit
    Add funcionamiento de los botones (Start y Exit)

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+
+
+    public void StartNew(){
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit(){
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    }
+}
